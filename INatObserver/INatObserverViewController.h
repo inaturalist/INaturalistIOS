3aacd095a6d7b7313e44b34eabb3dfa5888d22e0
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2012-02-13 14:57:13 -0800

    Initial Commit

---
+++ INatObserver/INatObserverViewController.h
@@ -0,0 +1,13 @@
+//
+//  INatObserverViewController.h
+//  INatObserver
+//
+//  Created by Ken-ichi Ueda on 2/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface INatObserverViewController : UIViewController
+
+@end
---
+++ INatObserver/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  INatObserver
+//
+//  Created by Ken-ichi Ueda on 2/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "INatObserverAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([INatObserverAppDelegate class]));
+    }
+}
