ab841dd3d97ab6101e9206a66a2be4305a131878
Alex Shepard <alex@meatfreezer.com>
2014-11-12 11:05:42 -0800

    disambuguator isn't a VC

--- INaturalistIOS/ExploreDisambiguator.h
+++ INaturalistIOS/ExploreDisambiguator.h
@@ -10,7 +10,7 @@
 
 typedef void(^DismbiguationChosen)(id choice);
 
-@interface ExploreDisambiguator : UIViewController <UITableViewDataSource,UITableViewDelegate>
+@interface ExploreDisambiguator : NSObject <UITableViewDataSource,UITableViewDelegate>
 
 @property NSString *title;
 @property NSString *message;
