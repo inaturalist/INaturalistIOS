028e7d5048f8176b7f536014655d2a173c959860
Alex Shepard <alex@meatfreezer.com>
2014-11-20 18:22:00 -0800

    scale g+ auth page on phones
    the authorization page isn't scaling correctly, so the user would have
    to hunt around to find the Accept button. for just the second page of g+
    login, and just for iPhones, inject a zoom of the body into the webview.

--- INaturalistIOS/GooglePlusAuthViewController.m
+++ INaturalistIOS/GooglePlusAuthViewController.m
@@ -20,4 +20,17 @@
     return nil;
 }
 
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    // the service login page is sized correctly
+    // but the authorization page doesn't scale correctly in the viewport on phones
+    if (([webView.request.URL.absoluteString rangeOfString:@"https://accounts.google.com/ServiceLogin"].location == NSNotFound) &&
+        [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        
+        [webView stringByEvaluatingJavaScriptFromString:@"document.body.style.zoom = 0.5;"];
+    }
+    
+    // allow our superclass to setup the necessary hooks to redirect back into the app after login
+    [super webViewDidFinishLoad:webView];
+}
+
 @end
