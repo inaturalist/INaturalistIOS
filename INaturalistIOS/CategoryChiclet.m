46f54d0ac52c571d4271bfc05bf5327d1a08f66f
Alex Shepard <alex@meatfreezer.com>
2015-03-27 15:00:53 -0700

    remove commented out code

--- INaturalistIOS/CategoryChiclet.m
+++ INaturalistIOS/CategoryChiclet.m
@@ -66,12 +66,4 @@
     return chiclet;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
-
 @end
