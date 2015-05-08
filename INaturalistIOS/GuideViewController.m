0dcc86bc6745acd9af6f2ac9f51bc4d491889e44
Alex Shepard <alex@meatfreezer.com>
2015-05-08 12:38:39 -0700

    update to latest SWRevealVC

--- INaturalistIOS/GuideViewController.m
+++ INaturalistIOS/GuideViewController.m
@@ -22,39 +22,17 @@
     }
 }
 
-static NSString * const SWSegueRearIdentifier = @"sw_rear";
-static NSString * const SWSegueFrontIdentifier = @"sw_front";
-static NSString * const SWSegueRightIdentifier = @"sw_right";
 // Override to set guide. Not too thrilled about this approach
-- (void)prepareForSegue:(SWRevealViewControllerSegue *)segue sender:(id)sender
+- (void)prepareForSegue:(SWRevealViewControllerSegueSetController *)segue sender:(id)sender
 {
     NSString *identifier = segue.identifier;
-    if ( [segue isKindOfClass:[SWRevealViewControllerSegue class]] && sender == nil )
-    {
-        if ( [identifier isEqualToString:SWSegueRearIdentifier] )
-        {
-            segue.performBlock = ^(SWRevealViewControllerSegue* rvc_segue, UIViewController* svc, UIViewController* dvc)
-            {
-                [self setRearViewController:dvc];
-            };
-        }
-        else if ( [identifier isEqualToString:SWSegueFrontIdentifier] )
-        {
-            segue.performBlock = ^(SWRevealViewControllerSegue* rvc_segue, UIViewController* svc, UIViewController* dvc)
-            {
-                GuideCollectionViewController *vc = (GuideCollectionViewController *)dvc;
-                vc.guide = self.guide;
-                [self setFrontViewController:vc];
-            };
-        }
-        else if ( [identifier isEqualToString:SWSegueRightIdentifier] )
-        {
-            segue.performBlock = ^(SWRevealViewControllerSegue* rvc_segue, UIViewController* svc, UIViewController* dvc)
-            {
-                GuideMenuViewController *vc = (GuideMenuViewController *)dvc;
-                vc.delegate = (GuideCollectionViewController *)self.frontViewController;
-                [self setRightViewController:dvc];
-            };
+    if ( [segue isKindOfClass:[SWRevealViewControllerSegueSetController class]] && sender == nil ) {
+        if ( [identifier isEqualToString:SWSegueFrontIdentifier] ) {
+            GuideCollectionViewController *vc = (GuideCollectionViewController *)segue.destinationViewController;
+            vc.guide = self.guide;
+        } else if ( [identifier isEqualToString:SWSegueRightIdentifier] ) {
+            GuideMenuViewController *vc = (GuideMenuViewController *)segue.destinationViewController;
+            vc.delegate = (GuideCollectionViewController *)self.frontViewController;
         }
     }
 }
