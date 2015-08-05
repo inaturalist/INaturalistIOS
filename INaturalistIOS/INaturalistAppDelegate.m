417e6faf90143f9a8bbd7c18ce2cd4b4fa8932a8
Alex Shepard <alex@inaturalist.org>
2015-08-05 15:22:32 -0700

    setup login controller at app launch, not in bg
    setting it up in the background was causing a crash during google+ login.

--- INaturalistIOS/INaturalistAppDelegate.m
+++ INaturalistIOS/INaturalistAppDelegate.m
@@ -64,6 +64,9 @@
 {
     [self setupAnalytics];
     
+    // we need a login controller to handle google auth, can't do this in the background
+    self.loginController = [[LoginController alloc] init];
+
     [self showLoadingScreen];
     
     [self configureApplicationInBackground];
@@ -109,8 +112,6 @@
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
         
         [self configureGlobalStyles];
-        
-        self.loginController = [[LoginController alloc] init];
         
         [self configureRestKit];
         [self configureOAuth2Client];
