971fe7ee334c6efa5c0e9538c9e4737150e91316
Alex Shepard <alex@inaturalist.org>
2015-09-04 19:59:48 -0700

    remove unused code

--- INaturalistIOS/DisclosureCell.m
+++ INaturalistIOS/DisclosureCell.m
@@ -76,14 +76,5 @@
     return self;
 }
 
-- (void)awakeFromNib {
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
 
 @end
