290f98a306e2bad2db2fd9847bc98f3a3a6f32ad
Alex Shepard <alex@inaturalist.org>
2016-01-10 17:07:24 -0800

    increase share icon and circle by 2pts each

--- INaturalistIOS/PhotosPageControlCell.m
+++ INaturalistIOS/PhotosPageControlCell.m
@@ -16,11 +16,11 @@
 - (void)awakeFromNib {
     // Initialization code
     
-    self.shareButton.layer.cornerRadius = 29.0 / 2;
+    self.shareButton.layer.cornerRadius = 31.0 / 2;
     self.shareButton.clipsToBounds = YES;
     self.shareButton.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.8f];
     
-    FAKIcon *share = [FAKIonIcons iosUploadOutlineIconWithSize:18];
+    FAKIcon *share = [FAKIonIcons iosUploadOutlineIconWithSize:20];
     [share addAttribute:NSForegroundColorAttributeName
                   value:[UIColor whiteColor]];
     [self.shareButton setAttributedTitle:share.attributedString
