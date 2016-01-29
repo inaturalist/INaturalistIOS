ef8fe13cf7ceb4b16c2c0eadeee383d68af87edb
Alex Shepard <alex@inaturalist.org>
2016-01-28 16:05:07 -0800

    user icons shouldn't get top/bottom padding

--- INaturalistIOS/NewsItemViewController.m
+++ INaturalistIOS/NewsItemViewController.m
@@ -30,7 +30,7 @@
 - (void)loadPostBodyIntoWebView {
     NSString *html = @"<head><meta name=\"viewport\" content=\"width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;\" /></head>";
 
-    html = [html stringByAppendingString:@"<body style=\"font-family: -apple-system, Helvetica, Arial, sans-serif; font-size: 17; \" ><style>div {max-width: 100%%; font-family=-apple-system, Helvetica, Arial, sans-serif; } figure { padding: 0; margin: 0; } img.user { border: 1px solid #C8C7CC; width: 20; height: 20; -webkit-border-radius: 50%%; margin-right: 4; margin-left: 7; vertical-align: middle; } img { padding-top: 4; padding-bottom: 4; max-width: 100%%; } p {font-family: -apple-system, Helvetica, Arial, sans-serif; } div.post { padding-left: 0; padding-right: 0; margin-left: 15; margin-right: 15; }</style><div class=\"post\">"];
+    html = [html stringByAppendingString:@"<body style=\"font-family: -apple-system, Helvetica, Arial, sans-serif; font-size: 17; \" ><style>div {max-width: 100%%; font-family=-apple-system, Helvetica, Arial, sans-serif; } figure { padding: 0; margin: 0; } img.user { padding-top: 0; padding-bottom: 0; border: 1px solid #C8C7CC; width: 20; height: 20; border-radius: 50%%; margin-right: 4; margin-left: 7; vertical-align: middle; } img { padding-top: 4; padding-bottom: 4; max-width: 100%%; } p {font-family: -apple-system, Helvetica, Arial, sans-serif; } div.post { padding-left: 0; padding-right: 0; margin-left: 15; margin-right: 15; }</style><div class=\"post\">"];
 
     NSString *title = self.newsItem.postTitle ?: NSLocalizedString(@"Untitled Post", nil);
     html = [html stringByAppendingString:[NSString stringWithFormat:@"<p style=\"font-size: 24; \">%@</p>", title]];
