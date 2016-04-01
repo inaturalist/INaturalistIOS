feeb236fd60504872c3fcd4372c99a8d64e8a8b8
Alex Shepard <alex@inaturalist.org>
2016-04-01 14:09:06 -0700

    don't reuse photoless obs without clearing photo

--- INaturalistIOS/ProjectObsPhotoCell.m
+++ INaturalistIOS/ProjectObsPhotoCell.m
@@ -6,8 +6,16 @@
 //  Copyright © 2016 iNaturalist. All rights reserved.
 //
 
+#import <SDWebImage/UIImageView+WebCache.h>
+
 #import "ProjectObsPhotoCell.h"
 
 @implementation ProjectObsPhotoCell
+
+- (void)prepareForReuse {
+    self.photoImageView.image = nil;
+    [self.photoImageView sd_cancelCurrentImageLoad];
+    self.obsText.text = @"";
+}
 
 @end
