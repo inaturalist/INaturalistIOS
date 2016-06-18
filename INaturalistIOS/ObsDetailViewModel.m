3041e2189144636da6b451b2cc77ec7cb59d7526
Alex Shepard <alex@inaturalist.org>
2016-06-18 13:00:48 -0700

    fix _another_ taxon locale display issue

--- INaturalistIOS/ObsDetailViewModel.m
+++ INaturalistIOS/ObsDetailViewModel.m
@@ -34,6 +34,7 @@
 #import "UIColor+ExploreColors.h"
 #import "INatPhoto.h"
 #import "UIImage+INaturalist.h"
+#import "NSLocale+INaturalist.h"
 
 @interface ObsDetailViewModel ()
 
@@ -261,6 +262,11 @@
         if ([[[RKClient sharedClient] reachabilityObserver] isNetworkReachable]) {
             
             NSString *resource = [NSString stringWithFormat:@"/taxa/%ld.json", (long)self.observation.taxonID.integerValue];
+            NSString *localeString = [NSLocale inat_serverFormattedLocale];
+            if (localeString && ![localeString isEqualToString:@""]) {
+                resource = [resource stringByAppendingFormat:@"?locale=%@", localeString];
+            }
+
             
             __weak typeof(self) weakSelf = self;
             RKObjectLoaderDidLoadObjectBlock taxonLoadedBlock = ^(id object) {
