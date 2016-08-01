50087c95deed013ee74de9f5526c8576fff6c808
Alex Shepard <alex@inaturalist.org>
2016-08-01 15:11:37 -0700

    remove id please from explore list view

--- INaturalistIOS/ExploreListTableViewCell.m
+++ INaturalistIOS/ExploreListTableViewCell.m
@@ -25,7 +25,7 @@ static NSDateFormatter *shortFormatter;
     
     UILabel *commonNameLabel;
     UILabel *scientificNameLabel;
-    UILabel *observationAttrLabel;  // ID Needed, or Research Grade
+    UILabel *observationAttrLabel;  // Research Grade
     UILabel *observerNameLabel;
     UILabel *observedOnLabel;
     
@@ -414,11 +414,6 @@ static NSDateFormatter *shortFormatter;
         observationAttrLabel.text = NSLocalizedString(@"RESEARCH", nil);
         observationAttrLabel.textColor = [UIColor colorForResearchGradeNotice];
         observationAttrLabel.backgroundColor = [UIColor secondaryColorForResearchGradeNotice];
-        observationAttrLabel.hidden = NO;
-    } else if (observation.idPlease) {
-        observationAttrLabel.text = NSLocalizedString(@"ID PLEASE", nil);
-        observationAttrLabel.textColor = [UIColor colorForIdPleaseNotice];
-        observationAttrLabel.backgroundColor = [UIColor secondaryColorForIdPleaseNotice];
         observationAttrLabel.hidden = NO;
     }
 }
