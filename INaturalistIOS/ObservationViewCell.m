865bf6a1fd1027690059d0df10f1ff094359f51a
Alex Shepard <alex@inaturalist.org>
2016-11-25 11:59:54 -0800

    we're using autolayout in the storyboard now

--- INaturalistIOS/ObservationViewCell.m
+++ INaturalistIOS/ObservationViewCell.m
@@ -10,39 +10,4 @@
 
 @implementation ObservationViewCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    
-    self.observationImage.translatesAutoresizingMaskIntoConstraints = NO;
-    self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    self.subtitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    self.titleLabel.textAlignment = NSTextAlignmentNatural;
-    self.subtitleLabel.textAlignment = NSTextAlignmentNatural;
-    
-    NSDictionary *views = @{
-                            @"imageView": self.observationImage,
-                            @"title": self.titleLabel,
-                            @"subtitle": self.subtitleLabel,
-                            };
-
-    
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.subtitleLabel
-                                                     attribute:NSLayoutAttributeLeading
-                                                     relatedBy:NSLayoutRelationEqual
-                                                        toItem:self.titleLabel
-                                                     attribute:NSLayoutAttributeLeading
-                                                    multiplier:1
-                                                      constant:0]];
-    
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-5-[imageView(==44)]-5-|"
-                                                                 options:NSLayoutFormatAlignAllLeading
-                                                                 metrics:0
-                                                                   views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-5-[title(==subtitle)]-2-[subtitle]-4-|"
-                                                                 options:0
-                                                                 metrics:0
-                                                                   views:views]];
-}
-
 @end
