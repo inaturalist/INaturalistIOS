db1ed03b6636fb966ac005d75eca95071a8a043b
Alex Shepard <alex@inaturalist.org>
2017-03-07 15:03:59 -0800

    base test class for obs detail view model

---
+++ iNaturalistTests/ObsDetailViewModelTests.m
@@ -0,0 +1,26 @@
+//
+//  ObsDetailViewModelTests.m
+//  iNaturalist
+//
+//  Created by Alex Shepard on 3/7/17.
+//  Copyright © 2017 iNaturalist. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ObsDetailViewModelTests : XCTestCase
+
+@end
+
+@implementation ObsDetailViewModelTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+
+@end
