63b0b502edfa8a079c603eed9fbec6c4ac06578f
Alex Shepard <alex@inaturalist.org>
2020-05-25 13:04:12 -0700

    build fix

--- INaturalistIOS/NewsItemViewController.m
+++ INaturalistIOS/NewsItemViewController.m
@@ -70,14 +70,14 @@
 
     html = [html stringByAppendingString:@"<body style=\"font-family: -apple-system, Helvetica, Arial, sans-serif; font-size: 17; \" ><style>div {max-width: 100%%; font-family=-apple-system, Helvetica, Arial, sans-serif; } figure { padding: 0; margin: 0; } img.user { padding-top: 0; padding-bottom: 0; border: 1px solid #C8C7CC; width: 20; height: 20; border-radius: 50%%; margin-right: 4; margin-left: 7; vertical-align: middle; } img { padding-top: 4; padding-bottom: 4; max-width: 100%%; } p {font-family: -apple-system, Helvetica, Arial, sans-serif; } div.post { padding-left: 0; padding-right: 0; margin-left: 15; margin-right: 15; }</style><div class=\"post\">"];
 
-    NSString *title = self.newsItem.postTitle ?: NSLocalizedString(@"Untitled Post", @"Title displayed for a journal post when the post has no title" );
+    NSString *title = self.post.postTitle ?: NSLocalizedString(@"Untitled Post", @"Title displayed for a journal post when the post has no title" );
     html = [html stringByAppendingString:[NSString stringWithFormat:@"<p style=\"font-size: 24; \">%@</p>", title]];
     
     NSString *postedBy = NSLocalizedString(@"Posted by", @"label for a news post author");
     NSString *authorIconURL = self.post.authorIconUrl;
     html = [html stringByAppendingString:[NSString stringWithFormat:@"<p style=\"font-size: 14; color: #686868;\">%@:<img class=\"user\" src=%@ />", postedBy, authorIconURL]];
-    NSString *author = self.newsItem.authorLogin ?: NSLocalizedString(@"Unknown author", @"Text shown in place of a post author when a post has no known author");
-    NSString *publishedAt = [[YLMoment momentWithDate:self.newsItem.postPublishedAt] fromNowWithSuffix:NO];
+    NSString *author = self.post.authorLogin ?: NSLocalizedString(@"Unknown author", @"Text shown in place of a post author when a post has no known author");
+    NSString *publishedAt = [[YLMoment momentWithDate:self.post.postPublishedAt] fromNowWithSuffix:NO];
     html = [html stringByAppendingString:[NSString stringWithFormat:@"%@  •  %@</p>", author, publishedAt]];
 
     html = [html stringByAppendingString:self.post.postBody];
