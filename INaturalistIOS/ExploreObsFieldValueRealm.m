a9b674f63553da8ae72b7af381c53a97c6a7e64f
Alex Shepard <alex@inaturalist.org>
2020-09-19 12:06:55 -0700

    don't upload ofvs without uuids or values

--- INaturalistIOS/ExploreObsFieldValueRealm.m
+++ INaturalistIOS/ExploreObsFieldValueRealm.m
@@ -127,7 +127,7 @@
 }
 
 - (NSDictionary *)uploadableRepresentation {
-    if (self.obsField && self.observation) {
+    if (self.obsField && self.observation && self.uuid && self.value) {
         return @{
             @"observation_field_value": @{
                     @"uuid": self.uuid,
