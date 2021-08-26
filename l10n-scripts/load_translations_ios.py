e72bd4b4784ab5242fde38ddeca7ca9225e8725e
Alex Shepard <alex@meatfreezer.com>
2021-08-26 09:08:52 -0700

    add l10n tools directly into the iNat iOS repo

---
+++ l10n-scripts/load_translations_ios.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+from pathlib import Path
+from shutil import copyfile
+
+ios_to_crowdin_lang_mappings = {
+    "ar": "ar",             # arabic
+    "bg": "bg",             # bulgarian
+    "cs": "cs",             # czech
+    "ca-ES": "ca",          # catalan
+    "da": "da",             # danish
+    "de": "de",             # german
+    "el": "el",             # greek
+    "es-MX": "es-MX",       # spanish (mexico)
+    "es": "es-ES",          # spanish (spain, other)
+    "fi-FI": "fi",          # finnish
+    "fr": "fr",             # french
+    "he": "he",             # hebrew
+    "id": "id",             # indonesian
+    "it": "it",             # italiano
+    "ja": "ja",             # japanese
+    "ko": "ko",             # korean
+    "lt": "lt",             # lithuanian
+    "lv": "lv",             # latvian
+    "nl": "nl",             # dutch
+    "nb": "nb",             # norwegian
+    "pt-BR": "pt-BR",       # portuguese (brazil)
+    "pt": "pt-BR",          # portuguese (portugal, other)
+    "ro": "ro",             # romanian
+    "ru": "ru",             # russian
+    "si": "si-LK",          # sinhala
+    "sv": "sv-SE",          # swedish
+    "th": "th",             # thai
+    "tr": "tr",             # turkish
+    "uk": "uk",             # ukrainian
+    "zh-Hans": "zh-CN",     # simplified chinese
+    "zh-Hant-HK": "zh-HK",  # traditional chinese (hong kong)
+    "zh-Hant-TW": "zh-TW",  # traditional chinese (taiwan)
+    "zh-Hant": "zh-TW",     # traditional chinese (other)
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--translations_dir', type=str, required=True,
+    help='directory of translations from crowdin'
+)
+parser.add_argument(
+    '--inat_ios_source_dir', type=str, required=True,
+    help='root directory of inaturalistios git project'
+)
+
+def main():
+    args = parser.parse_args()
+
+    for (ios, crowdin) in ios_to_crowdin_lang_mappings.items():
+        target_dir = Path(args.inat_ios_source_dir).joinpath("INaturalistIOS").joinpath(ios).with_suffix(".lproj")
+        source_dir = Path(args.translations_dir).joinpath(crowdin).joinpath("iOS")
+        source_files = os.listdir(source_dir)
+        for source_file in source_files:
+            source_full_path = Path(source_dir).joinpath(source_file)
+            target_full_path = Path(target_dir).joinpath(source_file)
+            copyfile(source_full_path, target_full_path)
+
+if __name__ == "__main__":
+    main()
---
+++ l10n-scripts/validate_translations_ios.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import localizable
+import os
+import re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--translations_dir', type=str, required=True,
+    help='directory of translations from crowdin'
+)
+
+
+def main():
+    args = parser.parse_args()
+
+    translations_basedir = args.translations_dir
+
+    # commonly used invalid matches
+        # %2$s
+    invalid_match = re.compile(r'%\d*\$s')
+
+    # realistic likely valid matches:
+        # %1$.5f
+        # %@
+        # %3$@
+        # %.5f
+    valid_match = re.compile(r'%\d\$\.\df|%@|%\d\$@|%\.\df')
+
+    for language in os.listdir(translations_basedir):
+        lang_basedir = os.path.join(translations_basedir, language)
+        lang_iosdir = os.path.join(lang_basedir, "iOS")
+
+        for stringsfile in os.listdir(lang_iosdir):
+            sf_path = os.path.join(lang_iosdir, stringsfile)
+            strings = localizable.parse_strings(filename=sf_path)
+            for string in strings:
+                # look for invalids
+                invalid_matches = re.findall(invalid_match, string["value"])
+                if invalid_matches:
+                    print(sf_path)
+                    print(string["key"])
+                    print(string["value"])
+                    print()
+
+                # assert matching valid matches in key and vlaue
+                valid_key_matches = re.search(valid_match, string["key"])
+                #if valid_key_matches:
+                #    print(valid_key_matches)
+                #print(string)
+                #if valmatches:
+                #    print(valmatches)
+
+if __name__ == "__main__":
+    main()
+
