86b910a25338c8a1360b09a8eb0db9693f3d746d
Alex Shepard <alex@meatfreezer.com>
2022-09-20 14:58:50 -0700

    adjust import to avoid a warning

--- INaturalistIOS/Models/Mantle/ExplorePost.m
+++ INaturalistIOS/Models/Mantle/ExplorePost.m
@@ -6,7 +6,7 @@
 //  Copyright © 2020 iNaturalist. All rights reserved.
 //
 
-#import <NSString_stripHtml/NSString_stripHTML.h>
+@import NSString_stripHtml;
 
 #import "ExplorePost.h"
 #import "NSURL+INaturalist.h"
