1e0c3f1d242090c79498cdabeade25a22dea4832
Alex Shepard <alex@meatfreezer.com>
2022-09-21 17:25:17 -0700

    make Activity formally an abstract class
    mostly to just clean up warnings

--- INaturalistIOS/Models/CoreData/Activity.m
+++ INaturalistIOS/Models/CoreData/Activity.m
@@ -10,4 +10,52 @@
 
 @implementation Activity
 
+/*
+ This is an abstract class, Comment and Identification implement these methods (via Core Data accessors).
+ */
+- (NSString *)body {
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+}
+
+- (void)setBody:(NSString *)body {
+    [NSException raise:NSInternalInconsistencyException
+                format:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)];
+}
+
+- (User *)user {
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+}
+
+- (void)setUser:(User *)user {
+    [NSException raise:NSInternalInconsistencyException
+                format:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)];
+}
+
+- (Observation *)observation {
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+}
+
+- (void)setObservation:(Observation *)observation {
+    [NSException raise:NSInternalInconsistencyException
+                format:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)];
+}
+
+
+- (NSDate *)createdAt {
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)]
+                                 userInfo:nil];
+}
+
+- (void)setCreatedAt:(NSDate *)createdAt {
+    [NSException raise:NSInternalInconsistencyException
+                format:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)];
+}
+
 @end
