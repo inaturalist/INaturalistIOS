5a72acd0b21cc18a9616d5b44739286ec0ff46eb
Alex Shepard <alex@meatfreezer.com>
2023-08-29 16:31:58 -0700

    use sf symbols for some image defaults
    work around what appears to be a crash in iOS 17
    also, modernize look

--- INaturalistIOS/Extensions/UIImage+INaturalist.m
+++ INaturalistIOS/Extensions/UIImage+INaturalist.m
@@ -15,41 +15,57 @@
 @implementation UIImage (INaturalist)
 
 + (instancetype)inat_defaultGuideImage {
-    static UIImage *defaultGuideImage;
-    if (!defaultGuideImage) {
-        defaultGuideImage = ({
-            FAKIcon *guideIcon = [FAKIonIcons iosBookOutlineIconWithSize:40];
-            [guideIcon addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithHexString:@"#8e8e93"]];
-            [guideIcon imageWithSize:CGSizeMake(40, 40)];
-        });
+    if (@available(iOS 13.0, *)) {
+        return [UIImage systemImageNamed:@"book"];
+    } else {
+        static UIImage *defaultGuideImage;
+
+        if (!defaultGuideImage) {
+            defaultGuideImage = ({
+
+                FAKIcon *guideIcon = [FAKIonIcons iosBookOutlineIconWithSize:40];
+                [guideIcon addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithHexString:@"#8e8e93"]];
+                [guideIcon imageWithSize:CGSizeMake(40, 40)];
+            });
+        }
+
+        return defaultGuideImage;
     }
-    return defaultGuideImage;
 }
 
 + (instancetype)inat_defaultProjectImage {
-    static UIImage *defaultProjectImage;
-    if (!defaultProjectImage) {
-        defaultProjectImage = ({
-            FAKIcon *projectIcon = [FAKIonIcons iosBriefcaseOutlineIconWithSize:80];
-            [projectIcon addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithHexString:@"#8e8e93"]];
-            [projectIcon imageWithSize:CGSizeMake(80, 80)];
-        });
+    if (@available(iOS 13.0, *)) {
+        return [UIImage systemImageNamed:@"briefcase"];
+    } else {
+        static UIImage *defaultProjectImage;
+
+        if (!defaultProjectImage) {
+            defaultProjectImage = ({
+                FAKIcon *projectIcon = [FAKIonIcons iosBriefcaseOutlineIconWithSize:80];
+                [projectIcon addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithHexString:@"#8e8e93"]];
+                [projectIcon imageWithSize:CGSizeMake(80, 80)];
+            });
+        }
+        return defaultProjectImage;
     }
-    return defaultProjectImage;
 }
 
 + (instancetype)inat_defaultUserImage {
-    static UIImage *defaultUserImage;
-    if (!defaultUserImage) {
-        defaultUserImage = ({
-            FAKIcon *person = [FAKIonIcons iosPersonIconWithSize:40];
-            [person addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor]];
-            FAKIcon *circle = [FAKIonIcons recordIconWithSize:60];
-            [circle addAttribute:NSForegroundColorAttributeName value:[UIColor lightGrayColor]];
-            [UIImage imageWithStackedIcons:@[ circle, person ] imageSize:CGSizeMake(60, 60)];
-        });
+    if (@available(iOS 13.0, *)) {
+        return [UIImage systemImageNamed:@"person.fill"];
+    } else {
+        static UIImage *defaultUserImage;
+        if (!defaultUserImage) {
+            defaultUserImage = ({
+                FAKIcon *person = [FAKIonIcons iosPersonIconWithSize:40];
+                [person addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor]];
+                FAKIcon *circle = [FAKIonIcons recordIconWithSize:60];
+                [circle addAttribute:NSForegroundColorAttributeName value:[UIColor lightGrayColor]];
+                [UIImage imageWithStackedIcons:@[ circle, person ] imageSize:CGSizeMake(60, 60)];
+            });
+        }
+        return defaultUserImage;
     }
-    return defaultUserImage;
 }
 
 
