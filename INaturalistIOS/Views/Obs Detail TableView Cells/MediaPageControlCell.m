a96267d6ada6b220c0a8bd821b364cce70332a39
Alex Shepard <alex@meatfreezer.com>
2023-09-21 13:24:00 -0700

    use sfsymbols here

--- INaturalistIOS/Views/Obs Detail TableView Cells/MediaPageControlCell.m
+++ INaturalistIOS/Views/Obs Detail TableView Cells/MediaPageControlCell.m
@@ -10,22 +10,17 @@
 @import AFNetworking;
 
 #import "MediaPageControlCell.h"
+#import "iNaturalist-Swift.h"
 
 @implementation MediaPageControlCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
-    
-    self.shareButton.layer.cornerRadius = 31.0 / 2;
-    self.shareButton.clipsToBounds = YES;
-    self.shareButton.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.8f];
-    
-    FAKIcon *share = [FAKIonIcons iosUploadOutlineIconWithSize:20];
-    [share addAttribute:NSForegroundColorAttributeName
-                  value:[UIColor whiteColor]];
-    [self.shareButton setAttributedTitle:share.attributedString
-                                forState:UIControlStateNormal];
+
+    UIImage *share = [UIImage iconImageWithSystemName:@"square.and.arrow.up.circle.fill" size:IconImageSizeMedium];
+    [self.shareButton setImage:share
+                      forState:UIControlStateNormal];
 
     self.shareButton.hidden = YES;
     self.shareButton.accessibilityLabel = NSLocalizedString(@"Share", nil);
